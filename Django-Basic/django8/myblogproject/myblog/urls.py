c8b4dd12009d584d74bb7b8544bf988960333771
Kim-SuBin <sb2504@naver.com>
2020-09-16 22:26:09 +0900

    [Update] urls.py
    주석 설정 변경

--- Django-Basic/django8/myblogproject/myblog/urls.py
+++ Django-Basic/django8/myblogproject/myblog/urls.py
@@ -1,4 +1,4 @@
-#기본 path 함수를 사용하기 위해 똑같이 해당 기능을 import
+# 기본 path 함수를 사용하기 위해 똑같이 해당 기능을 import
 from django.urls import path
 
 # 프로젝트에서는 어떤 app의 views인지 알기 위해 blog.views 앱부터 명시했지만
@@ -12,7 +12,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # Index
-    ## 프로젝트의 urls 와의 차이점 - App 내의 view 인지 명시하는 부분이 빠짐
+    # 프로젝트의 urls 와의 차이점 - App 내의 view 인지 명시하는 부분이 빠짐
     path('', views.home, name='home'),
     path('create10/', views.create10, name='create10'),
     path('<int:post_id>/', views.detail, name="detail"),
