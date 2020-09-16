a421accd09fb4a445ce0de5e2801d1087c4c997a
Kim-SuBin <sb2504@naver.com>
2020-09-17 00:30:09 +0900

    [Update] models.py
    Add like_users

--- Django-Basic/django8/myblogproject/myblog/models.py
+++ Django-Basic/django8/myblogproject/myblog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
@@ -7,13 +8,13 @@ class Post(models.Model):
     image = models.ImageField(upload_to='images/', null=True, blank=True)
     pub_date = models.DateTimeField('date published')
     body = models.TextField()
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_posts')
 
     def __str__(self):
         return self.title
 
     def summary(self):
         return self.body[:20]
-
 
 class Comment(models.Model):
     post = models.ForeignKey('myblog.Post', on_delete = models.CASCADE)
