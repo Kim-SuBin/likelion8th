44b93b91bdb51c58050d04273f1279d0e1ce59c9
Kim-SuBin <sb2504@naver.com>
2020-09-19 20:01:02 +0900

    [Update] login&out 기능 구현
    login&out 기능 구현

--- Django-Basic/django9/login/accounts/views.py
+++ Django-Basic/django9/login/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect 
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 def signup(request):
     if request.method == 'POST':
@@ -9,10 +10,21 @@ def signup(request):
     return render(request, 'signup.html')
     
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None: 
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'login.html', {'error': 'username or password is incorrect.'})
+    else:
+        return render(request, 'login.html')
 
 def logout(request):
-    return render(request, 'logout.html')
+    auth.logout(request)
+    return redirect('index')
 
 def index(request):
     return render(request, 'index.html')
