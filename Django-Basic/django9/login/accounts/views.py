8f7c4392585ab0ff9bb15f054e4779557d0f2726
Kim-SuBin <sb2504@naver.com>
2020-09-19 19:43:19 +0900

    [Add] Django 실습 기본 환경 구축
    Django 실습 기본 환경 구축

---
+++ Django-Basic/django9/login/accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def signup(request):
+    return render(request, 'signup.html')
+    
+def login(request):
+    return render(request, 'login.html')
+
+def logout(request):
+    return render(request, 'logout.html')
+
+def index(request):
+    return render(request, 'index.html')
